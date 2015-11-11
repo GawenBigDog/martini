af0e0f58ee091aec3e63850fe58b30f9eafa59d3
Shule Liu <shuleliu@midway-login1.rcc.local>
2015-11-10 22:36:17 -0600

    Corrected a bug in parameter generation script

--- gen_para_cap.py
+++ gen_para_cap.py
@@ -9,13 +9,18 @@
 # the length of lipid is 1.95 = 3.9/2 originally
 # here I used larger value to avoid steric collision
 
+# modified to read lipid PDB files that puts lipid on the lattice
+
 import math
 
 from math import pi,sqrt,acos,sin,cos
 
+d_water = 0.47    # diameter of water bead: 0.47 nm
+lipid_len = d_water*7.0   # for current lipids, the longest length is 0.47*7 nm 
+
 f = open('cylpara_cap.txt','w')
 
-R = float(raw_input("Cylinder Radius, in unit of nm, should be larger than 8.0 nm: "))
+R = float(raw_input("Cylinder Radius, in unit of nm, should be larger than %.3f nm: " %(lipid_len*2.0)))
 
 L = float(raw_input("Length of Cylinder, in unit of nm: "))
 
@@ -25,12 +30,21 @@ c_salt=float(raw_input("salt concentration in unit of mol/L: "))
 
 water_thickness = float(raw_input("water thickness on one side of the tube in unit of nm: "))
 
+cap_args = raw_input("With cap or not? (Y/N)")
+
+cap_bool=True
+if 'Y' in cap_args or 'y' in cap_args:
+   cap_bool=True
+elif 'N' in cap_args or 'n' in cap_args:
+   cap_bool=False
+
+
 while Lz/2.0<=R:
       print "Twice radius is %f" %(2.0*R)
       print "Lz must be greater than this value"
       Lz=float(raw_input("Reenter Lz in unit of nm: "))
 
-r = R - 8.0  #radius of water cylinder
+r = R - lipid_len*2.0  #radius of water cylinder
 
 print>>f, "Radius of cylinder: %f nm" %R
 print>>f, "Length of cylinder: %f nm" %L
@@ -38,20 +52,24 @@ print>>f, "Length in the z direction: %f nm" %Lz
 print>>f, "Radius of water cylinder: %f nm" %r
 print>>f, "Radius of the center of the lipid bilayer: %f nm" %((R+r)/2.0)
 
-Lx = L + 2.0*R + 2.0*water_thickness
+if cap_bool:
+   Lx = L + 2.0*R + 2.0*water_thickness
+else:
+   Lx = L  + 2.0*water_thickness
+
 print>>f, "Estimated Lx in unit of nm: %.5f" %Lx
 
-surf_area = 1.0  # GCER/DPGS surface area
+surf_area = d_water*d_water*4  # estimated surface area based on the DPGS lattice PDB
 
 n_outer = int(2.0*math.pi*R*L/surf_area)
-n_inner = int(2.0*math.pi*(R+r)/2.0*L/surf_area)
+n_inner = int(2.0*math.pi*r*L/surf_area)
 
 print>>f, "Number of lipids in outer leaflet: %d" %n_outer
 print>>f, "Number of lipids in inner leaflet: %d" %n_inner
 
 # calculate the number of lipids in the cap
 # this is manually manipulated to reduce the cap density
-n_outer_cap = int(4.0*pi*(r+4.0)*(r+4.0)/surf_area)
+n_outer_cap = int(4.0*pi*R*R/surf_area)
 n_inner_cap = int(4.0*pi*r*r/surf_area)
 
 print>>f, "Number of lipids in outer leaflet of the cap: %d" %n_outer_cap
@@ -59,9 +77,9 @@ print>>f, "Number of lipids in inner leaflet of the cap: %d" %n_inner_cap
 
 l_lipid = sqrt(surf_area)
 
-d_costheta_outer = l_lipid/(r+4.0)
+d_costheta_outer = l_lipid/R
 nbintheta_cap_outer = int(2.0/d_costheta_outer) + 1
-d_phi_outer = l_lipid/(r+4.0)
+d_phi_outer = l_lipid/R
 nbinphi_cap_outer = int(2.0*pi/d_phi_outer) + 1
 
 # make sure there is enough grids
@@ -97,7 +115,11 @@ print>>f,"Number of bins in phi for inner cap: %d" %nbinphi_cap_inner
 r_water = 0.47/2.0   # radius of the martini water
 v_water = 4.0/3.0*math.pi*r_water**3
 n_water_inner = int(2.0*math.pi*r*L/v_water)
-n_water_outer = int((Lz*Lz*Lx - 2.0*pi*R*R*L - 4.0/3.0*pi*R*R*R)/v_water)
+if cap_bool:
+   n_water_outer = int((Lz*Lz*Lx - pi*R*R*L - 4.0/3.0*pi*R*R*R)/v_water)
+else:
+   n_water_outer = int((Lz*Lz*Lx - pi*R*R*L)/v_water)
+
 print>>f, "Number of water inside the vesicle: %d" %n_water_inner
 print>>f, "Number of water outside the vesicle: %d" %n_water_outer
 
@@ -121,7 +143,7 @@ print>>f,"\n"
 # start writing the input parameter part
 
 #DPGS
-pdbname='DPGS-em.pdb'
+pdbname='DPGS_lattice.pdb'
 n_i = n_inner/6
 n_o = n_outer/6
 n_i_c = n_inner_cap/6
@@ -130,7 +152,7 @@ print>>f, "%-20s %5d %5d %5d %5d" %(pdbname,n_i,n_o,n_o_c,n_i_c)
 
  
 #POPE
-pdbname='POPE-em.pdb'
+pdbname='POPE_lattice.pdb'
 n_i = n_inner/6
 n_o = n_outer/6
 n_i_c = n_inner_cap/6
@@ -138,7 +160,7 @@ n_o_c = n_outer_cap/6
 print>>f, "%-20s %5d %5d %5d %5d" %(pdbname,n_i,n_o,n_o_c,n_i_c)
 
 #CHOL
-pdbname='CHOL-em.pdb'
+pdbname='CHOL_lattice.pdb'
 n_i = n_inner/3
 n_o = n_outer/3
 n_i_c = n_inner_cap/3
@@ -146,7 +168,7 @@ n_o_c = n_outer_cap/3
 print>>f, "%-20s %5d %5d %5d %5d" %(pdbname,n_i,n_o,n_o_c,n_i_c)
 
 #DPPC
-pdbname='DPPC-em.pdb'
+pdbname='DPPC_lattice.pdb'
 n_i = n_inner/3
 n_o = n_outer/3
 n_i_c = n_inner_cap/3
@@ -154,6 +176,7 @@ n_o_c = n_outer_cap/3
 print>>f, "%-20s %5d %5d %5d %5d" %(pdbname,n_i,n_o,n_o_c,n_i_c)
 
 print>>f,"output   cyl_py_cap.gro"
+print>>f,"cap_bool %s" %(str(cap_bool))
 print>>f,"r_inner  %.4f" %r
 print>>f,"r_outer  %.4f" %R
 print>>f,"Lx       %.4f" %L     # note the we print L rather than Lx
